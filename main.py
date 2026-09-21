4f390e3eb8c818832d446150964782934b7650d1
RicardoBitt <ricardobittenoliver@gmail.com>
2026-09-21 15:04:24 -0300

    mudança dee linguagem no código principal

---
+++ main.py
@@ -0,0 +1,95 @@
+import customtkinter as ctk
+# from PIL import Image
+#import io
+#import base64
+
+
+
+class app(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Image Enhancer")
+
+        self.geometry("1280x720")
+
+        self.minsize(1000, 600)
+
+        ctk.set_appearance_mode("dark")
+
+        ctk.set_default_color_theme("blue")
+
+
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
+
